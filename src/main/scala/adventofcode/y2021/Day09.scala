62202eb477875064602aeaa7941f728f719a8514
Johan Walters <johan.walters@visma.com>
2021-12-13 12:38:04 +0100

    final tweaks day9

--- src/main/scala/adventofcode/y2021/Day09.scala
+++ src/main/scala/adventofcode/y2021/Day09.scala
@@ -1,11 +1,22 @@
 package adventofcode.y2021
 
+// functor typeclass: all things mappable
+trait Functor[F[_]] {
+  extension[A](a: F[A]) def fmap[B](f: A => B): F[B]
+}
+
+given Functor[Seq] with
+  extension[A](a: Seq[A]) def fmap[B](f: A => B): Seq[B] = a.map(f)
+
 // comonadic store
 case class Store[S,A](peek: S => A, cursor: S):
   def extract: A = peek(cursor)
   def extend[B](f: Store[S,A] => B): Store[S, B] = Store(s => f(Store(peek, s)), cursor)
   def map[B](f: A => B): Store[S,B] = extend(s => f(s.extract))
   def zip[B](other: Store[S, B]): Store[S, (A, B)] = Store(s => (peek(s), other.peek(s)), cursor)
+  def experiment[F[_]: Functor](fn: S => F[S]): F[A] = fn(cursor).fmap(peek)
+
+type Grid[A] = Store[Vect2D[Int], A]
 
 @main def Day09 = withResource("day09a.txt") {
 
@@ -20,36 +31,37 @@ case class Store[S,A](peek: S => A, cursor: S):
   val surroundings: Seq[Vect2D[Int]] =
     Seq(-1, 1).flatMap(i => Seq(Vect2D(i, 0), Vect2D(0, i))) // up, down, left, right
 
-  val surroundingStore: Store[Vect2D[Int], Seq[Vect2D[Int]]] =
-    depthStore.extend { store =>
+  def withSurroundings[A](store: Grid[A]): (A, Seq[A]) =
+    store.extract -> store.experiment { c =>
       surroundings
-        .map(_ + store.cursor)
+        .map(_ + c)
         .filter { case Vect2D(x, y) =>
           x >= 0 && x < grid(0).size && y >= 0 && y < grid.size // keep only surroundings that exist
         }
     }
 
-  // local depth, and surroundings with their depths
-  val surroundingsWithDepthsStore: Store[Vect2D[Int], (Int, Seq[(Vect2D[Int], Int)])] =
-    depthStore zip surroundingStore.map(_.map(s => s -> depthStore.peek(s)))
+  val depthSurroundingsStore: Store[Vect2D[Int], (Int, Seq[Int])] =
+    depthStore.extend(withSurroundings)
 
-  val lowPoints = allPoints.flatMap(v => surroundingsWithDepthsStore.peek(v) match {
-    case (depth, surr) => Option.when(surr.forall(_._2 > depth))(v -> depth)
-  })
+  val lowPointsStore: Store[Vect2D[Int], Option[Int]] =
+    depthSurroundingsStore.map { case (depth, surr) => Option.when(surr.forall(_ > depth))(depth) }
 
-  val resultA = lowPoints.map(_._2 + 1).sum
+  val (lowPoints, lowPointsPositions) = allPoints.flatMap(p => lowPointsStore.peek(p).map(_ -> p)).unzip
+
+  val resultA = lowPoints.map(_ + 1).sum
 
   println(resultA)
 
-  val uphillStore = surroundingsWithDepthsStore.map { case (centerDepth, surr) =>
-    surr.collect { case (s, d) if d > centerDepth && d < 9 => s }
-  }
+  val uphillStore: Store[Vect2D[Int], Seq[Vect2D[Int]]] =
+    depthStore.zip(depthStore.extend(_.cursor)).extend(withSurroundings).map {
+      case ((centerDepth, _), surr) =>
+        surr.collect { case (d, s) if d > centerDepth && d < 9 => s }
+    }
 
-  val basins = lowPoints.map(_._1).map { lp =>
-    Set(lp).unfold { start =>
+  val basins = lowPointsPositions.map { lp =>
+    Set(lp).unfold { start => // repeatedly walk up until no more new points.
       val all = start ++ start.flatMap(uphillStore.peek)
-      val newPoints = all -- start
-      Either.cond(newPoints.isEmpty, all, all) // done when empty, 'all' is both stop condition and next interation
+      Either.cond((all -- start).isEmpty, all, all) // done when empty, 'all' is both stop condition and next iteration
     }
   }
 
@@ -58,6 +70,7 @@ case class Store[S,A](peek: S => A, cursor: S):
 
   // part II
   println(product)
+
 
 
 }
